f667e2154ed867f7102e3b807034304ac87c8580
Kevin Qu <kevinqu88@gmail.com>
2024-04-09 23:52:51 -0400

    tpying

--- app/shared/links/NavLink.tsx
+++ app/shared/links/NavLink.tsx
@@ -1,14 +1,23 @@
 'use client';
 
-import Link from "next/link";
+import Link from 'next/link';
 import styles from './NavLink.module.css';
-import { usePathname } from "next/navigation";
-import type { ReactNode } from "react";
+import { usePathname } from 'next/navigation';
+import type { ReactNode } from 'react';
+import type { Route } from 'next';
 
-export default function NavLink({ url, label, children }: Readonly<{ url: string; label: string; children: ReactNode }>) {
+export default function NavLink({
+  url,
+  label,
+  children,
+}: Readonly<{ url: string; label: string; children: ReactNode }>) {
   const pathName = usePathname();
   return (
-    <Link href={ `${url}` } className={ `${'font-sans'} ${pathName.startsWith(url) ? `font-bold ${styles.active}` : undefined}` } title={ `${label}` }>
+    <Link
+      href={ url as Route }
+      className={ `${'font-sans'} ${pathName.startsWith(url) ? `font-bold ${styles.active}` : undefined}` }
+      title={ `${label}` }
+    >
       { children }
     </Link>
   );
